99edb7e0e85cea28760a8df1b59825db5f350b66
Estellairon <66968160+Estellarium@users.noreply.github.com>
2022-08-26 17:41:08 -0400

    Add files via upload

---
+++ mains/BlocoNotas/main/java/com/example/meuprojeto/BotaoJogo.java
@@ -0,0 +1,25 @@
+package com.example.meuprojeto;
+import android.graphics.Color;
+import android.widget.Button;
+
+public class BotaoJogo extends Button{
+    private boolean ligado = false;
+    private BotaoJogo[] vizinhos = {null, null, null, null};
+
+    public BotaoJogo() {}
+
+    public void addNeigh(BotaoJogo[] btn) {
+        for (int i = 0; i < 4; i++)
+            vizinhos[i] = btn[i];
+    }
+
+    public void trocaCor(boolean apertado) {
+        if (ligado) {this.setBackgroundColor(Color.YELLOW); ligado = false;}
+        else {this.setBackgroundColor(Color.BLUE); ligado = true;}
+
+        if (apertado)
+            for (BotaoJogo x:vizinhos)
+                if (x!=null)
+                    x.trocaCor(false);
+    }
+}
